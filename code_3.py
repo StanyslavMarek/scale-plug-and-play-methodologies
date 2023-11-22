ade05255c2f9f76471b3aa19e3702289d4ce5ea3
StanyslavMarek <140631407+StanyslavMarek@users.noreply.github.com>
2023-11-22 15:16:22 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,17 @@
+
+}
+main();
+    data.forEach(item => {
+    const data = generateRandomData();
+function main() {
+        console.log("Random Number: " + item);
+        data.push(Math.floor(Math.random() * 100) + 1);
+    return data;
+function generateRandomData() {
+
+    for (let i = 0; i < 10; i++) {
+    }
+    const data = [];
+
+}
+    });
