f7bfb6c19a6221bdf15019428647b917173f7e18
StanyslavMarek <140631407+StanyslavMarek@users.noreply.github.com>
2023-12-06 14:01:18 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,65 +1,26 @@
-    data = generate_random_data()
-
-        print(f"Random Number: {item}")
-def main():
-    main()
-import random
-def generate_random_data():
-
 
     for item in data:
-if __name__ == "__main__":
-    data = [random.randint(1, 100) for _ in range(10)]
-
-    return data    for item in data:
-
-    data = [random.randint(1, 100) for _ in range(10)]
-
-        print(f"Random Number: {item}")
-
-
-    main()
-import random
-    data = generate_random_data()
 def main():
+
+def generate_random_data():
+import random
     return data
 if __name__ == "__main__":
-def generate_random_data():            data[i] = (int) (Math.random() * 100) + 1;
-        for (int i = 0; i < 10; i++) {
-public class RandomDataGenerator {
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
 
-        }
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+            data[i] = (int) (Math.random() * 100) + 1;
         for (int item : data) {
+        int[] data = new int[10];
+
+
     }
     public static void main(String[] args) {
         }
 }
-        int[] data = new int[10];
-
-            System.out.println("Random Number: " + item);        print(f"Random Number: {item}")
-    return data
-import random
-
-
-if __name__ == "__main__":
-
-    main()
-
-    for item in data:
-def generate_random_data():
-    data = generate_random_data()
-def main():
-    data = [random.randint(1, 100) for _ in range(10)]    return data
-def main():
-    data = [random.randint(1, 100) for _ in range(10)]
-import random
-
-
-
-    data = generate_random_data()
-    main()
-if __name__ == "__main__":
-        print(f"Random Number: {item}")
-
-def generate_random_data():
-    for item in data:
+        for (int i = 0; i < 10; i++) {
+        }
+            System.out.println("Random Number: " + item);
+public class RandomDataGenerator {
